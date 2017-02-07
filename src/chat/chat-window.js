47015e014510b77a7c249a78037f38f698b00875
deskach <dzianis.kachanau@gmail.com>
2017-02-07 12:00:07 +1100

    laid the ground for the chat message

--- src/chat/chat-window.js
+++ src/chat/chat-window.js
@@ -19,7 +19,28 @@ export default class MessageWindow extends Component {
           <header className="cursor">
             <strong>{this.props.title}</strong>
           </header>
-          <div id="content"></div>
+          <div id="content">
+            {/*<p>*/}
+            {/*Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at ante. Mauris eleifend, quam a vulputate dictum, massa quam dapibus leo, eget vulputate orci purus ut lorem. In fringilla mi in ligula. Pellentesque aliquam quam vel dolor. Nunc adipiscing. Sed quam odio, tempus ac, aliquam molestie, varius ac, tellus. Vestibulum ut nulla aliquam risus rutrum interdum. Pellentesque lorem. Curabitur sit amet erat quis risus feugiat viverra. Pellentesque augue justo, sagittis et, lacinia at, venenatis non, arcu. Nunc nec libero. In cursus dictum risus. Etiam tristique nisl a nulla. Ut a orci. Curabitur dolor nunc, egestas at, accumsan at, malesuada nec, magna.*/}
+            {/*Nulla facilisi. Nunc volutpat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Ut sit amet orci vel mauris blandit vehicula. Nullam quis enim. Integer dignissim viverra velit. Curabitur in odio. In hac habitasse platea dictumst. Ut consequat, tellus eu volutpat varius, justo orci elementum dolor, sed imperdiet nulla tellus ut diam. Vestibulum ipsum ante, malesuada quis, tempus ac, placerat sit amet, elit.*/}
+            {/*</p>*/}
+            <div className="chat-message">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at ante. Mauris eleifend, quam a vulputate
+              dictum, massa quam dapibus leo, eget vulputate orci purus ut lorem. In fringilla mi in ligula.
+              Pellentesque aliquam quam vel dolor. Nunc adipiscing. Sed quam odio, tempus ac, aliquam molestie, varius
+              ac, tellus. Vestibulum ut nulla aliquam risus rutrum interdum. Pellentesque lorem. Curabitur sit amet erat
+              quis risus feugiat viverra. Pellentesque augue justo, sagittis et, lacinia at, venenatis non, arcu. Nunc
+              nec libero. In cursus dictum risus. Etiam tristique nisl a nulla. Ut a orci. Curabitur dolor nunc, egestas
+              at, accumsan at, malesuada nec, magna.
+            </div>
+            <div className="chat-message">
+              Nulla facilisi. Nunc volutpat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
+              cubilia Curae; Ut sit amet orci vel mauris blandit vehicula. Nullam quis enim. Integer dignissim viverra
+              velit. Curabitur in odio. In hac habitasse platea dictumst. Ut consequat, tellus eu volutpat varius, justo
+              orci elementum dolor, sed imperdiet nulla tellus ut diam. Vestibulum ipsum ante, malesuada quis, tempus
+              ac, placerat sit amet, elit.
+            </div>
+          </div>
           <footer>
             <input type="text" placeholder="Type here..."/>
             <span className="pointer send">&#x27a4;</span>
